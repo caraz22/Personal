28ba461a49d33b37a6d89aac8c45f1203c746a4a
caraz22 <cszoke22@gmail.com>
2023-01-12 23:20:03 -0500

    Finished Finances.java

--- Finances.java
+++ Finances.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Finances {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Scanner in = new Scanner(System.in);
 
@@ -12,21 +12,58 @@ public class Finances {
 
         double rent = 450.00;
         double dashPass = 4.99;
-        double creditPayment = 35.00;
+        double creditMinimum = 35.00;
         double xboxLive = 16.19;
         double studentPrime = 8.00;
         double regularPrime = 16.00;
         double spotify = 4.99;
         double internet = 69.99;
+        double groceries = 160.00;
+        
+        double total = rent + dashPass + creditMinimum + xboxLive + studentPrime + regularPrime + spotify + internet + groceries;
         
         ArrayList<String> paymentDates = new ArrayList<>();
-        System.out.println(date(month, "rent", 1, ("$" + round(rent))));
+        String rentPayment = date(month, "rent", 1, round(rent));
+        String dashPassPayment = date(month, "DashPass", 2, round(dashPass));
+        String creditPayment = date(month, "minimum credit payment", 1, round(creditMinimum));
+        String xboxPayment = date(month, "Xbox Live", 5, round(xboxLive));
+        String studentPrimePayment = date(month, "Amazon Prime for students", 9, round(studentPrime));
+        String regularPrimePayment = date(month, "Amazon Prime", 12, round(regularPrime));
+        String spotifyPayment = date(month, "Spotify premium", 19, round(spotify));
+        String internetPayment = date(month, "Spectrum internet", 20, round(internet));
+        paymentDates.add(rentPayment);
+        paymentDates.add(dashPassPayment);
+        paymentDates.add(creditPayment);
+        paymentDates.add(xboxPayment);
+        paymentDates.add(studentPrimePayment);
+        paymentDates.add(regularPrimePayment);
+        paymentDates.add(spotifyPayment);
+        paymentDates.add(internetPayment);
+
+        for (int i = 0; i < paymentDates.size(); i++) {
+            System.out.println(paymentDates.get(i));
+        }
+
+        System.out.println("You will also pay for groceries a couple times, around $80 per trip");
+        System.out.println();
+
+        System.out.println("Your monthly payments come out to be " + round(total));
+        System.out.println();
+
+        double loan = 5060.00;
+        double monthlyAmount = loan / 4;
+        double freeSpend = monthlyAmount - total;
+
+
+        System.out.println("You will be receiving " + round(loan) + " to last for about 4 months");
+        System.out.println("This gives you roughly " + round(monthlyAmount) + " per month");
+        System.out.println("With all necessary payments being accounted for, you'll have about " + round(freeSpend) + " to spend on whatever you want");
 
         in.close();
     }
 
     public static String round(double charge) {
-        return String.format("%.2f", charge);
+        return "$" + String.format("%.2f", charge);
     }
 
     public static String date(String month, String charge, int day, String price) {
