3271e2a9549ed04e2bf784215dafa31975578d79
caraz22 <cszoke22@gmail.com>
2024-01-31 10:32:31 -0500

    trying to compute current financial state

--- money/spring24.py
+++ money/spring24.py
@@ -1,5 +1,4 @@
 startingAmount = 5700
-creditCard = 250
 laundry = 41.34
 ultimate = 18.35
 nitro = 2.99
@@ -12,12 +11,10 @@ aes = 90
 rent = 950
 pc = 60.71
 
-afterCredit = startingAmount - creditCard
 monthly = laundry + ultimate + nitro + primeStudent + netflix + spotify + insurance + youtube + aes + rent + pc
 january = monthly - rent
 restOfMonths = monthly * 3
 
-leftOver = afterCredit - january - restOfMonths
-freeMonthly = leftOver / 4
-print("Total left over is " + str(round(leftOver, 2)))
-print("Total left over per month is " + str(round(freeMonthly, 2)))
+print(str(round(monthly, 2)))
+print(str(round(january, 2)))
+print(str(round(restOfMonths, 2)))
