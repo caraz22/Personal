5460639242eda0c48e081c9a7afa34696e1f9df8
caraz22 <cszoke22@gmail.com>
2024-10-13 07:43:23 -0400

    added type images

--- GUI/dinner.java
+++ GUI/dinner.java
@@ -1,22 +1,22 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
-public class Dinner {
+// public class Dinner {
 
-    public static void main(String[] args) {
-        ArrayList<Object> meals = new ArrayList<Object>();      
-        CookingNight meal1 = new CookingNight("Alfredo");  
-        meals.add(meal1);
-        DateNight meal2 = new DateNight("Olive Garden");
-        meals.add(meal2);
+//     public static void main(String[] args) {
+//         ArrayList<Object> meals = new ArrayList<Object>();      
+//         CookingNight meal1 = new CookingNight("Alfredo");  
+//         meals.add(meal1);
+//         DateNight meal2 = new DateNight("Olive Garden");
+//         meals.add(meal2);
 
-        for (int i = 0; i < meals.size(); i++) {
-            System.out.println(meals.get(i));
-        }
-    }
+//         for (int i = 0; i < meals.size(); i++) {
+//             System.out.println(meals.get(i));
+//         }
+//     }
 
 
 
-    Dinner() {
+//     Dinner() {
 
-    }
-}
+//     }
+// }
